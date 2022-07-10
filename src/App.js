47196fa6e4bf6662119fd692f1865d5b6b4e0906
Sachidanand <s.shahmlg@gmail.com>
2022-07-10 17:46:22 +0530

    Css Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import ReactTooltip from 'react-tooltip'
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <button data-for = "tool" data-tip="Thanks for hover over me <br/> i'm Tooltip!">Hover over me!</button>
-        <ReactTooltip place='right' id='tool' effect='solid' multiline={true} />
+        <ReactTooltip place="right" id="tool" type="dark" effect="solid" multiline={true}/>
     </div>
   );
 }
